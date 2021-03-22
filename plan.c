74c286834d2e3cb2cedcaaa623942e86da682e5a
peach99cpp <1831427532@qqq.com>
2021-03-22 22:32:40 +0800

    移动的函数封装

--- plan.c
+++ plan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #define p_lock getchar()
+int x = 10, y = 10;
 void printf_move(int mode, int movement)
 {
     if (mode == 0) //x
@@ -14,16 +15,38 @@ void printf_move(int mode, int movement)
             printf(" ");
     }
 }
+void move_mode(void)
+{
+    scanf("%c", &input_mode);
+    switch (input_mode)
+    {
+    case 's':
+        x--;
+        break;
+    case 'a':
+        y--;
+        break;
+    case 'w':
+        x++;
+        break;
+    case 'd':
+        y++;
+        break;
+    }
+    x <= 0 ? x = 0 : x;
+    y <= 0 ? y = 0 : y;
+}
 int main()
 {
     //童晶老师的飞机游戏代码
-    int x = 10, y = 20;
+    char input_mode;
     while (1)
     {
         system("cls");
         printf_move(0, x);
         printf_move(1, y);
         printf("  **  \n");
+        move_mode();
     }
     p_lock;
     return 0;
