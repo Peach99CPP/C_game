6fe2cf22a7d71d979584e10b6695f3cda7a41aba
peach99cpp <1831427532@qqq.com>
2021-04-01 20:31:57 +0800

    输入输出控制系列

--- cow_num.cpp
+++ cow_num.cpp
@@ -4,8 +4,32 @@ using namespace std;
 
 int main()
 {
-    int a,b,c;
-    cin >> a >> b >> c;
-    printf("%-8d%-8d%-8d\n", a, b, c);//利用格式化输出进行控制，在C++中用的是setw()函数
-    return 0;
+    char ch;
+    cin >> ch;
+    printf("  %c  \n", ch);
+    printf(" %c%c%c \n", ch, ch, ch);
+    for (int i = 0; i < 5;++i)
+    {
+        printf("%c", ch);
+    }
+
+        // double temp;
+        // cin >> temp;
+        // printf("%f\n%.5f\n%e\n%g", temp,temp,temp, temp);
+
+        // char ch;
+        // int num;
+        // float f1;
+        // double f2;
+        // cin >> ch >> num >> f1 >> f2;
+        // printf("%c %4d %.2f %.12f", ch, num, f1, f2);
+
+        // double x;
+        // cin >> x;
+        // printf("%.12lf", x);
+
+        // float x;
+        // cin >> x;
+        // printf("%.3f", x);
+        return 0;
 }
