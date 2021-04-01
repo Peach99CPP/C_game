cf5f9261a350c9b4298d8703de0f04ac423554a3
peach99cpp <1831427532@qqq.com>
2021-04-01 20:33:40 +0800

    周长面积问题

--- cow_num.cpp
+++ cow_num.cpp
@@ -4,32 +4,8 @@ using namespace std;
 
 int main()
 {
-    char ch;
-    cin >> ch;
-    printf("  %c  \n", ch);
-    printf(" %c%c%c \n", ch, ch, ch);
-    for (int i = 0; i < 5;++i)
-    {
-        printf("%c", ch);
-    }
-
-        // double temp;
-        // cin >> temp;
-        // printf("%f\n%.5f\n%e\n%g", temp,temp,temp, temp);
-
-        // char ch;
-        // int num;
-        // float f1;
-        // double f2;
-        // cin >> ch >> num >> f1 >> f2;
-        // printf("%c %4d %.2f %.12f", ch, num, f1, f2);
-
-        // double x;
-        // cin >> x;
-        // printf("%.12lf", x);
-
-        // float x;
-        // cin >> x;
-        // printf("%.3f", x);
-        return 0;
+    int w, h;
+    cin >> w >> h;
+    printf("C:%d\nS:%d", 2 * (w + h), w * h);
+    return 0;
 }
